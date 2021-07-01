b108834e36ebf6c265ef82133b0d929e7e44ff9e
Shaunak Joshi <shauwnak.joshi@gmail.com>
2021-07-01 18:01:29 +0530

    Update convertVis_to_xml.py

--- convertVis_to_xml.py
+++ convertVis_to_xml.py
@@ -3,17 +3,19 @@
 import cv2
 import os
 import numpy as np
-from tqdm import tqdm 
+from tqdm import tqdm
+from PIL import Image
 
-input_img_folder = 'VisDrone2019-DET-train/images'
-input_ann_folder = 'VisDrone2019-DET-train/annotations'
-output_ann_folder = 'VisDrone2019-DET-train/annotations_new'
-output_img_folder = 'VisDrone2019-DET-train/images_new'
+input_img_folder = 'VisDrone2019-DET-val/images'
+input_ann_folder = 'VisDrone2019-DET-val/annotations'
+output_ann_folder = 'VisDrone2019-DET-val/annotations_new'
+output_img_folder = 'VisDrone2019-DET-val/images_new'
 
-scale = 2  # PROBABLY USELESS
-sizing = 1080  # scale image to this value
-# try 1024, 1920, 2048
+# scale = 2  # PROBABLY USELESS
+sizing = 1280  # scale image to this value
+# try 1024, 1080, 1920, 2048
 show_annotations = False # set to True to test
+
 
 os.makedirs(output_img_folder, exist_ok=True)
 os.makedirs(output_ann_folder, exist_ok=True)
@@ -41,11 +43,22 @@ count = 0
 
 
 def upscale_img(sizing, img):
-	h, w, c = img.shape
-	x_scale, y_scale = sizing / w, sizing / h
+	# h, w, c = img.shape
+	# height, width = np.array(im).shape
+	# w, h = img.size
+	# print(h, w)
+	# x_scale, y_scale = sizing / w, sizing / h
+	# up_scaled_img = img.resize((sizing, sizing))
 	# up_scaled_img = cv2.resize(img, dsize=(w*scale, h*scale), interpolation=cv2.INTER_LINEAR)
-	up_scaled_img = cv2.resize(img, dsize=(sizing, sizing), interpolation=cv2.INTER_LINEAR)
+	# up_scaled_img = cv2.resize(img, dsize=(sizing, sizing), interpolation=cv2.INTER_LINEAR)
+	up_scaled_img, x_scale, y_scale = img, 1, 1
 	return up_scaled_img, x_scale, y_scale
+
+
+# def normalize_img(img, sizing):
+# 	norm_img = np.zeros((sizing, sizing))
+# 	normalized_image = cv2.normalize(img,  norm_img, 0, 255, cv2.NORM_MINMAX)
+# 	return normalized_image
 
 
 def object_string(label, bbox):
@@ -73,9 +86,11 @@ for annotation in tqdm(annotation_list):
 	img_file = annotation.split('.txt')[0] + '.jpg'
 	img_path = os.path.join(os.getcwd(), input_img_folder, img_file)
 	output_img_path = os.path.join(os.getcwd(), output_img_folder, img_file)
-	img = cv2.imread(img_path)
+	img = Image.open(img_path)
+	# img = cv2.imread(img_path)
 	# scaling the image by the sizing value
 	img, x_scale, y_scale = upscale_img(sizing, img)
+	# img = normalize_img(img, sizing)
 	annotation_string_init = '''
 <annotation>
 	<folder>annotations</folder>
@@ -89,7 +104,7 @@ for annotation in tqdm(annotation_list):
 		<height>{}</height>
 		<depth>{}</depth>
 	</size>
-	<segmented>0</segmented>'''.format(img_file, img_path, img.shape[1], img.shape[0], img.shape[2])
+	<segmented>0</segmented>'''.format(img_file, img_path, img.size[0], img.size[1], len(img.mode))
 
 	file = open(annotation_path, 'r')
 	lines = file.readlines()
@@ -106,11 +121,12 @@ for annotation in tqdm(annotation_list):
 		annotation_string_init = annotation_string_init + req_str
 		if show_annotations:
 			cv2.rectangle(img, new_coords_min, new_coords_max, color, thickness)
-	cv2.imwrite(output_img_path, img)
+	# cv2.imwrite(output_img_path, img)
+	img.save(output_img_path)
 	annotation_string_final = annotation_string_init + '</annotation>'
 	f = open(xml_path, 'w')
 	f.write(annotation_string_final)
 	f.close()
 	count += 1
 
-print('[INFO] Completed {} image(s) and annotation(s) pair & Upscaled + Normalized to : {} x {}'.format(count, sizing, sizing))
+print('[INFO] Completed {} image(s) and annotation(s) pair & Upscaled to : {} x {}'.format(count, sizing, sizing))
