4200a141bade1063768cbf594090fb46997b6e76
Shaunak Joshi <shauwnak.joshi@gmail.com>
2021-07-02 14:51:08 +0530

    Update convertVis_to_xml.py

--- convertVis_to_xml.py
+++ convertVis_to_xml.py
@@ -1,27 +1,9 @@
 # convert Visdrone to PASCAL-VOC
-
-import cv2
 import os
-import numpy as np
 from tqdm import tqdm
-from PIL import Image
+from PIL import Image, ImageDraw
+import argparse
 
-input_img_folder = 'VisDrone2019-DET-val/images'
-input_ann_folder = 'VisDrone2019-DET-val/annotations'
-output_ann_folder = 'VisDrone2019-DET-val/annotations_new'
-output_img_folder = 'VisDrone2019-DET-val/images_new'
-
-# scale = 2  # PROBABLY USELESS
-sizing = 1280  # scale image to this value
-# try 1024, 1080, 1920, 2048
-show_annotations = False # set to True to test
-
-
-os.makedirs(output_img_folder, exist_ok=True)
-os.makedirs(output_ann_folder, exist_ok=True)
-
-image_list = os.listdir(input_img_folder)
-annotation_list = os.listdir(input_ann_folder)
 
 # removed the 0,11 keys, since they are useless
 label_dict = {
@@ -37,28 +19,12 @@ label_dict = {
 	"10": "Motor"
 }
 
-thickness = 2
-color = (255, 0, 0)
-count = 0
-
 
 def upscale_img(sizing, img):
-	# h, w, c = img.shape
-	# height, width = np.array(im).shape
-	# w, h = img.size
-	# print(h, w)
-	# x_scale, y_scale = sizing / w, sizing / h
-	# up_scaled_img = img.resize((sizing, sizing))
-	# up_scaled_img = cv2.resize(img, dsize=(w*scale, h*scale), interpolation=cv2.INTER_LINEAR)
-	# up_scaled_img = cv2.resize(img, dsize=(sizing, sizing), interpolation=cv2.INTER_LINEAR)
-	up_scaled_img, x_scale, y_scale = img, 1, 1
+	w, h = img.size
+	x_scale, y_scale = sizing / w, sizing / h
+	up_scaled_img = img.resize((sizing, sizing))
 	return up_scaled_img, x_scale, y_scale
-
-
-# def normalize_img(img, sizing):
-# 	norm_img = np.zeros((sizing, sizing))
-# 	normalized_image = cv2.normalize(img,  norm_img, 0, 255, cv2.NORM_MINMAX)
-# 	return normalized_image
 
 
 def object_string(label, bbox):
@@ -79,54 +45,98 @@ def object_string(label, bbox):
 	return req_str
 
 
-for annotation in tqdm(annotation_list):
-	annotation_path = os.path.join(os.getcwd(), input_ann_folder, annotation)
-	xml_annotation = annotation.split('.txt')[0] + '.xml'
-	xml_path = os.path.join(os.getcwd(), output_ann_folder, xml_annotation)
-	img_file = annotation.split('.txt')[0] + '.jpg'
-	img_path = os.path.join(os.getcwd(), input_img_folder, img_file)
-	output_img_path = os.path.join(os.getcwd(), output_img_folder, img_file)
-	img = Image.open(img_path)
-	# img = cv2.imread(img_path)
-	# scaling the image by the sizing value
-	img, x_scale, y_scale = upscale_img(sizing, img)
-	# img = normalize_img(img, sizing)
-	annotation_string_init = '''
-<annotation>
-	<folder>annotations</folder>
-	<filename>{}</filename>
-	<path>{}</path>
-	<source>
-		<database>Unknown</database>
-	</source>
-	<size>
-		<width>{}</width>
-		<height>{}</height>
-		<depth>{}</depth>
-	</size>
-	<segmented>0</segmented>'''.format(img_file, img_path, img.size[0], img.size[1], len(img.mode))
+def convert(args):
+	count = 0
 
-	file = open(annotation_path, 'r')
-	lines = file.readlines()
-	for line in lines:
-		new_line = line.strip('\n').split(',')
-		new_coords_min = (int(int(new_line[0]) * x_scale), int(int(new_line[1]) * y_scale))
-		new_coords_max = (int(int(new_line[0]) * x_scale) + int((int(new_line[2]) * x_scale)),
-						  int((int(new_line[1]) * y_scale)) + int(int(new_line[3]) * y_scale))
-		bbox = (int(int(new_line[0]) * x_scale), int(int(new_line[1]) * y_scale),
+	# try 1024, 1080, 1920, 2048
+
+	os.makedirs(args.output_img_folder, exist_ok=True)
+	os.makedirs(args.output_ann_folder, exist_ok=True)
+
+	annotation_list = os.listdir(args.input_ann_folder)
+
+	for annotation in tqdm(annotation_list):
+		annotation_path = os.path.join(os.getcwd(), args.input_ann_folder, annotation)
+		xml_annotation = annotation.split('.txt')[0] + '.xml'
+		xml_path = os.path.join(os.getcwd(), args.output_ann_folder, xml_annotation)
+		img_file = annotation.split('.txt')[0] + '.jpg'
+		img_path = os.path.join(os.getcwd(), args.input_img_folder, img_file)
+		output_img_path = os.path.join(os.getcwd(), args.output_img_folder, img_file)
+		img = Image.open(img_path)
+		# scaling the image by the sizing value
+		if args.sizing != None:
+			img, x_scale, y_scale = upscale_img(int(args.sizing), img)
+		else:
+			x_scale, y_scale = 1, 1
+		annotation_string_init = '''
+	<annotation>
+		<folder>annotations</folder>
+		<filename>{}</filename>
+		<path>{}</path>
+		<source>
+			<database>Unknown</database>
+		</source>
+		<size>
+			<width>{}</width>
+			<height>{}</height>
+			<depth>{}</depth>
+		</size>
+		<segmented>0</segmented>'''.format(img_file, img_path, img.size[0], img.size[1], len(img.mode))
+
+		file = open(annotation_path, 'r')
+		lines = file.readlines()
+		for line in lines:
+			new_line = line.strip('\n').split(',')
+			new_coords_min = (
+				int(int(new_line[0]) * x_scale), int(int(new_line[1]) * y_scale)
+			)
+			new_coords_max = (
+				int(int(new_line[0]) * x_scale) + int((int(new_line[2]) * x_scale)),
+				int((int(new_line[1]) * y_scale)) + int(int(new_line[3]) * y_scale)
+			)
+			bbox = (
+				int(int(new_line[0]) * x_scale), int(int(new_line[1]) * y_scale),
 				int(int(new_line[0]) * x_scale) + int(int(new_line[2]) * x_scale),
-				int(int(new_line[1]) * y_scale) + int(int(new_line[3]) * y_scale))
-		label = label_dict.get(new_line[5])
-		req_str = object_string(label, bbox)
-		annotation_string_init = annotation_string_init + req_str
-		if show_annotations:
-			cv2.rectangle(img, new_coords_min, new_coords_max, color, thickness)
-	# cv2.imwrite(output_img_path, img)
-	img.save(output_img_path)
-	annotation_string_final = annotation_string_init + '</annotation>'
-	f = open(xml_path, 'w')
-	f.write(annotation_string_final)
-	f.close()
-	count += 1
+				int(int(new_line[1]) * y_scale) + int(int(new_line[3]) * y_scale)
+			)
+			label = label_dict.get(new_line[5])
+			req_str = object_string(label, bbox)
+			annotation_string_init = annotation_string_init + req_str
 
-print('[INFO] Completed {} image(s) and annotation(s) pair & Upscaled to : {} x {}'.format(count, sizing, sizing))
+			if args.show_annotations:
+				img1 = ImageDraw.Draw(img)
+				img1.rectangle([new_coords_max, new_coords_min], outline="blue")
+
+		img.save(output_img_path)
+		annotation_string_final = annotation_string_init + '</annotation>'
+		f = open(xml_path, 'w')
+		f.write(annotation_string_final)
+		f.close()
+		count += 1
+
+	print('[INFO] Completed {} image(s) and annotation(s) pair & Upscaled to : {} x {}'.format(count, args.sizing, args.sizing))
+
+
+def main():
+	parser = argparse.ArgumentParser(
+		description='This script support converting voc format xmls to coco format json')
+	parser.add_argument(
+		'--input_img_folder', type=str, default=None, help='path to img files directory')
+	parser.add_argument(
+		'--input_ann_folder', type=str, default=None, help='path to annotation files ')
+	parser.add_argument(
+		'--output_img_folder', type=str, default=None, help='path of output img')
+	parser.add_argument(
+		'--output_ann_folder', type=str, default=None, help='path to output xml ann')
+	parser.add_argument(
+		'--sizing', type=str, default=None, help='upscale the image to given res. ; None means no upscaling')
+	parser.add_argument(
+		'--show_annotations', type=str, default=False, help='shows markings')
+
+	args = parser.parse_args()
+
+	convert(args)
+
+
+if __name__ == '__main__':
+	main()
